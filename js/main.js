cdbdda601a0e4cdea4fdb482ff2b879c6ad08f98
Gulnoza <91131639+G12251@users.noreply.github.com>
2023-05-20 13:27:21 +0500

    Revert "feat: owl carousel"

--- js/main.js
+++ js/main.js
@@ -25,26 +25,3 @@ $(".owl-carousel").owlCarousel({
   },
 });
 
-$('.navigation-container').owlCarousel({
-  items:4,
-  loop:false,
-  center:true,
-  margin:10,
-  URLhashListener:true,
-  autoplayHoverPause:true,
-  startPosition: 'URLHash',
-  responsive: {
-    0: {
-      items: 1,
-    },
-    600: {
-      items: 2,
-    },
-    1000: {
-      items: 3,
-    },
-  },
-});
-
-
-
